9df7544b8c3af047734e3bfcd3b58c5691ebaef6
Onur Gunduz <ogunduz@gmail.com>
2017-02-12 23:26:36 +0100

    move meta-model singleton into "editor" folder

--- src/editor/add-value.tsx
+++ src/editor/add-value.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import {EditorState, FocusType} from "./state";
 import {preventBubbleUp} from "./utils/ui-util";
 
-import {default as metaModelInstance} from "../ide/instance"; // TODO: nope
+import {default as metaModelInstance} from "./instance"; // TODO: nope
 const sTypes = metaModelInstance.sTypes();
 
 const styles = require("./styles.scss");
