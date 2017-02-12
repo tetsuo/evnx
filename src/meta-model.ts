7dba9c341e75c8f109117a2653b63b1f07badac7
Onur Gunduz <ogunduz@gmail.com>
2017-02-12 01:55:53 +0100

    factor-out 'template'

--- src/meta-model.ts
+++ src/meta-model.ts
@@ -1,6 +1,5 @@
 import {util} from "./util";
-const isInteger = require("is-integer");
-const indentString = require("indent-string");
+import template = require("./template");
 
 export function buildMetaModel(model: IMetaModel): MetaModel {
 	return new MetaModel(model);
@@ -172,37 +171,6 @@ export class SemanticsNode {
 		return this.template(...this.getTemplateArguments()) + "\n";
 	}
 }
-
-export namespace template {
-
-	export function tag(strings: any, ...keys: any[]) {
-		return (function(...values: any[]) {
-			const dict = values[values.length - 1] || {};
-			const result = [strings[0]];
-
-			keys.forEach((key, i) => {
-				const value = isInteger(key) ? values[key] : dict[key];
-				result.push(value, strings[i + 1]);
-			});
-
-			return result.join("");
-		});
-	}
-
-	export function indent(s: string, n = 2, p = " ") {
-		return indentString(String(s), n, p);
-	}
-
-	export function capitalize(s: string) {
-		return s
-			.replace(/-/g, " ")
-			.replace(/\w\S*/g, res => {
-				return res.charAt(0).toUpperCase() + res.substr(1).toLowerCase();
-			})
-			.replace(/[\s\xa0]+/g, "");
-	}
-
-} /* template */
 
 const tag = template.tag;
 
